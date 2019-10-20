3599ba12b6da0fc5c6b5ad2dfe1d4aeeec22ccd2
DiegoArvz <darvz.gtz@gmail.com>
2019-10-19 22:06:57 -0500

    Project Setup

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    env: {
+      browser: true,
+      es6: true,
+      commonjs: true,
+    },
+    extends: 'eslint:recommended',
+    globals: {
+      Atomics: 'readonly',
+      SharedArrayBuffer: 'readonly',
+    },
+    parserOptions: {
+      ecmaVersion: 2018,
+      sourceType: 'module',
+    },
+    rules: {},
+  };
