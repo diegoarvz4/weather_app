ecc8ad2c3a91c3fb0a52873b300d70d7202673a5
DiegoArvz <darvz.gtz@gmail.com>
2019-10-19 22:40:49 -0500

    Function weather implemented

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log("test");
+
+document.getElementById('fetch-weather').addEventListener('click', (e) => {
+  e.preventDefault();
+  const city = document.getElementById('city-input').value.toLowerCase();
+  console.log(city)
+  fetchData(city);
+});
+
+const fetchData = (city, code) =>{
+  const APIKEY = 'dd0f409a2c224dba43acfef73f2eec39';
+  const endpoint = `api.openweathermap.org/data/2.5/weather?q=${city},${code}&APPID=${APIKEY}`;
+  fetch(endpoint, {mode: 'cors'})
+  .then(function(response) {
+    console.log(response)
+  })
+  .then(function(response) {
+    console.log(response)
+  });
+}
+
