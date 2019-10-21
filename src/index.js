c8000e797ce4b1617d50b36d4959e14be9a158bb
DiegoArvz <darvz.gtz@gmail.com>
2019-10-20 23:18:04 -0500

    Fetch method implemented

--- src/index.js
+++ src/index.js
@@ -2,19 +2,17 @@
 document.getElementById('fetch-weather').addEventListener('click', (e) => {
   e.preventDefault();
   const city = document.getElementById('city-input').value.toLowerCase();
-  console.log(city)
   fetchData(city);
 });
 
-const fetchData = (city, code) =>{
+const  fetchData = async (city) =>{
   const APIKEY = 'dd0f409a2c224dba43acfef73f2eec39';
-  const endpoint = `api.openweathermap.org/data/2.5/weather?q=${city},${code}&APPID=${APIKEY}`;
-  fetch(endpoint, {mode: 'cors'})
-  .then(function(response) {
-    console.log(response)
-  })
-  .then(function(response) {
-    console.log(response)
-  });
+  const endpoint = `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${APIKEY}&units=metric`;
+  const response = await fetch(endpoint, {mode: 'cors'});
+  const myJson = await response.json();
+  console.log(myJson);
+  console.log("Difference with utc: ", (myJson.timezone / 3600));
+  console.log(myJson.weather[0].main);
 }
 
+const weather = ['Mist', 'Clear', 'Clouds', 'Drizzle'];
