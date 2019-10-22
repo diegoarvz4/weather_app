b9f73c2cd9a763d98aef593d1a9971417c2392e7
DiegoArvz <darvz.gtz@gmail.com>
2019-10-22 18:01:06 -0500

    Final touches

--- src/index.js
+++ src/index.js
@@ -1,50 +1,60 @@
 
 import fromUnixTime from 'date-fns/fromUnixTime'
-import getUnixTime from 'date-fns/getUnixTime'
 
-document.getElementById('fetch-weather').addEventListener('click', (e) => {
-  e.preventDefault();
-  const city = document.getElementById('city-input').value.toLowerCase();
-  fetchData(city);
+document.getElementById('city-input').addEventListener('keydown', (e) => {
+  
+  if (e.keyCode === 13) {
+    e.preventDefault();
+    document.getElementById('stats').innerHTML = '<div id="loader" class="lds-dual-ring set-hidden"></div>';
+    document.getElementById('loader').classList.toggle('set-hidden');
+    const city = document.getElementById('city-input').value.toLowerCase();
+    fetchData(city);
+  }
 });
+
+
 
 const  fetchData = async (city) =>{
   const APIKEY = 'dd0f409a2c224dba43acfef73f2eec39';
   const endpoint = `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${APIKEY}&units=metric`;
   const response = await fetch(endpoint, {mode: 'cors'});
   const myJson = await response.json();
-  console.log(myJson);
-  console.log("Difference with utc: ", (myJson.timezone / 3600));
-  console.log(myJson.weather[0].main);
- // console.log(Math.floor((new Date()).getTime() / 1000));
-  const sunrise = fromUnixTime(myJson.sys.sunrise ); // city sunrise
-  const sunset = fromUnixTime(myJson.sys.sunset); //city sunset
-   //get Time
-   const localTime = new Date();
-   const utcTime = localTime.toUTCString();
-   const utcTimeDate = new Date(utcTime.substring(0,utcTime.length-4));
-   const cityTime = fromUnixTime(utcTimeDate.getTime() / 1000 + myJson.timezone)
-  console.log(fromUnixTime(myJson.sys.sunset) > cityTime )
-  if ( cityTime < sunset){
-    setBackground('day');
-  } else {
-    setBackground('night');
-  }
-  setStats({cityTime});
+  const weather = myJson.weather[0].main;
+  const weather_desc =  myJson.weather[0].description;
+  const temperature = myJson.main.temp;
+  const humidity = myJson.main.humidity;
+  const wind = myJson.wind.speed;
+  const clouds = myJson.clouds.all;
+  const time = new Date(myJson.dt);
+  const name = myJson.name +", "+ myJson.sys.country;
+  const code = myJson.sys.country;
+  const weatherIconURL = myJson.weather[0].icon;
+  const localTime = new Date();
+  const utcTime = localTime.toUTCString();
+  const utcTimeDate = new Date(utcTime.substring(0,utcTime.length-4));
+  const cityTime = new Date(fromUnixTime(utcTimeDate.getTime() / 1000 + myJson.timezone));
+  setStats({cityTime, weather, weather_desc, temperature, humidity, time, name, weatherIconURL, wind, clouds, code });
 }
 
-const setBackground = (status) => {
-  console.log('here')
-  const bg = document.getElementById('container');
-  console.log(bg.classList);
-  bg.classList.remove('day');
-  bg.classList.remove('night');
-  bg.classList.add(`${status}`);
-}
 
 const setStats = (stats) => {
   const statsContainer = document.getElementById('stats');
-  statsContainer.innerHTML = `<h1>${stats.cityTime}</h1>`;
+  statsContainer.innerHTML = `<div id="loader" class="lds-dual-ring set-hidden"></div>
+                              <div class="stats-header">
+                                <div class="stats-header-city">
+                                  <h1>${stats.name} </h1><img src="http://openweathermap.org/images/flags/${stats.code.toLowerCase()}.png"/>
+                                </div>
+                                <h4>${stats.cityTime.toString().substring(0,21)}</h4>
+                                <h4 style="text-transform: capitalize;">${stats.weather_desc}</h4>
+                              </div>
+                              <div class="stats-body">
+                                <span>${stats.temperature} °C</span> 
+                                <img  src ="http://openweathermap.org/img/wn/${stats.weatherIconURL}@2x.png" />
+                              </div>
+                              <div class="stats-other">
+                                <h5>Humidity ${stats.humidity}%</h5>
+                                <h5>Wind ${stats.wind} m/s </h5>
+                                <h5>Clouds ${stats.clouds}% </h5>
+                              </div>
+  `;
 }
-
-const weather = ['Mist', 'Clear', 'Clouds', 'Drizzle'];
